fd446d79f21ffb0e3dc695a716db403c9e8b4a25
preeti kumari kushwaha <kushwahapreeti999@gmail.com>
2021-04-04 21:06:28 +0530

    update

--- application/controllers/Forgot_password.php
+++ application/controllers/Forgot_password.php
@@ -42,6 +42,47 @@ class Forgot_password extends CI_Controller {
 		$this->load->view('Template2/Footer.php');
 	}
 
+	public function user_forgot_password()
+	{
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('username','Username', 'required');
+		$this->form_validation->set_rules('new_password','New Password', 'required');
+		$this->form_validation->set_rules('confirm_password','Confirm Password', 'required|matches[new_password]');
+
+		if($this->form_validation->run())
+		{
+
+			$this->load->model('User_Forgot_pwd_model');
+
+			$username = $this->input->post('username');
+            $new_password = $this->input->post('new_password');
+            $confirm_password = $this->input->post('confirm_password');
+
+            $query=$this->db->query("select password from login where user_name='$username' ");
+
+           	$row=$query->row();
+
+            if((!strcmp($new_password, $confirm_password)))
+            {
+                $this->User_Forgot_pwd_model->change_pass($username,$new_password);
+
+                $this->session->set_flashdata('success', "Password changed successfully");
+            }
+            else
+            {
+                   $this->session->set_flashdata('eror', "Invalid");
+            }
+
+		}
+	
+
+		$this->load->view('Template2/Header.php');
+		$this->load->view('Forgot_password.php');
+		$this->load->view('Template2/Footer.php');
+	}
+
+	}
+
 
 
 }
