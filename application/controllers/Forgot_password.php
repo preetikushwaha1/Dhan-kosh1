d5876cccca16f7046b914cd43be9679b0fd28cfe
preeti kumari kushwaha <kushwahapreeti999@gmail.com>
2021-04-05 12:38:16 +0530

    update

--- application/controllers/Forgot_password.php
+++ application/controllers/Forgot_password.php
@@ -1,7 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Forgot_password extends CI_Controller {
+class Forgot_password extends CI_Controller 
+{
 
 	public function index()
 	{
@@ -42,47 +43,55 @@ class Forgot_password extends CI_Controller {
 		$this->load->view('Template2/Footer.php');
 	}
 
+
+
+
+
+
+
+
 	public function user_forgot_password()
 	{
+		
+		$this->load->view('Template2/Header.php');
+		$this->load->view('Customer_pages/Customer_forgot_pwd.php');
+		$this->load->view('Template2/Footer.php');
+	}
+
+
+
+	public function user_forgot_password_change()
+	{
 		$this->load->library('form_validation');
-		$this->form_validation->set_rules('username','Username', 'required');
+		$this->form_validation->set_rules('acc_no','Account Number', 'required');
 		$this->form_validation->set_rules('new_password','New Password', 'required');
 		$this->form_validation->set_rules('confirm_password','Confirm Password', 'required|matches[new_password]');
 
 		if($this->form_validation->run())
 		{
 
-			$this->load->model('User_Forgot_pwd_model');
+			$this->load->model('Forgot_pwd_model');
 
-			$username = $this->input->post('username');
+			$account_no = $this->input->post('acc_no');
             $new_password = $this->input->post('new_password');
             $confirm_password = $this->input->post('confirm_password');
 
-            $query=$this->db->query("select password from login where user_name='$username' ");
-
-           	$row=$query->row();
+            $query=$this->db->query("select password,confirm_password from customer_details where account_no='$account_no' ");
 
             if((!strcmp($new_password, $confirm_password)))
             {
-                $this->User_Forgot_pwd_model->change_pass($username,$new_password);
+                $this->Forgot_pwd_model->change_customer_pass($account_no,$new_password,$confirm_password);
 
                 $this->session->set_flashdata('success', "Password changed successfully");
             }
-            else
+           	else
             {
                    $this->session->set_flashdata('eror', "Invalid");
             }
-
-		}
-	
-
+		
+	}
 		$this->load->view('Template2/Header.php');
-		$this->load->view('Forgot_password.php');
+		$this->load->view('Customer_pages/Customer_forgot_pwd.php');
 		$this->load->view('Template2/Footer.php');
 	}
-
-	}
-
-
-
 }
--- application/models/Forgot_pwd_model.php
+++ application/models/Forgot_pwd_model.php
@@ -9,5 +9,11 @@ class Forgot_pwd_model extends CI_Model
 	{
 		$update_pass=$this->db->query("UPDATE login set password='$new_password'  where user_name='$username'");
 	}
+
+	function change_customer_pass($account_no,$new_password,$confirm_password)
+	{
+		$update_pass=$this->db->query("UPDATE customer_details set password='$new_password' , confirm_password='$confirm_password'  where account_no='$account_no'");
+	
+	}
 }
 ?>
